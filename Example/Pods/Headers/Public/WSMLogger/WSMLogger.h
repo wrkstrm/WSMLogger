191d6a27dfa9931380e5f7045322744565e944b6
Cristian Monterroza <cmonterroza@wrkstrm.me>
2014-09-29 15:49:16 -0700

    Add WSMLogger files.

---
+++ Example/Pods/Headers/Public/WSMLogger/WSMLogger.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/WSMLogger.h
---
+++ Example/Pods/Target Support Files/Pods-WSMLogger-WSMLogger/Pods-WSMLogger-WSMLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WSMLogger_WSMLogger : NSObject
+@end
+@implementation PodsDummy_Pods_WSMLogger_WSMLogger
+@end
